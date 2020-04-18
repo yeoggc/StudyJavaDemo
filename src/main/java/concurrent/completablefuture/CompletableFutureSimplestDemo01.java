d2282a081ec54b6130309d20aa66ad45f93949c8
官国成 <guanguocheng365@gmail.com>
2020-04-18 11:37:43 +0800

    使用Future01

--- src/main/java/concurrent/completablefuture/CompletableFutureSimplestDemo01.java
+++ src/main/java/concurrent/completablefuture/CompletableFutureSimplestDemo01.java
@@ -1,13 +1,16 @@
 package concurrent.completablefuture;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class CompletableFutureSimplestDemo01 {
 
     public static void main(String[] args) throws InterruptedException {
 
         //创建异步执行任务
-        CompletableFuture<Double> cf = CompletableFuture.supplyAsync(CompletableFutureSimplestDemo01::fetchPrice);
+        CompletableFuture<Double> cf =
+                //fetchPrice()静态方法的签名符合Supplier接口的定义，使用Java8 方法引用特性 进行简化。
+                CompletableFuture.supplyAsync(CompletableFutureSimplestDemo01::fetchFund);
 
 
         cf.thenAccept((result) -> {// 如果执行成功
@@ -17,18 +20,18 @@ public class CompletableFutureSimplestDemo01 {
             return null;
         });
         // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
-        Thread.sleep(2000);
+        TimeUnit.SECONDS.sleep(2);
     }
 
-    static Double fetchPrice() {
+    static Double fetchFund() {
+
         try {
-            Thread.sleep(1000);
+            TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        if (Math.random() < 0.3) {
-            throw new RuntimeException("fetch price failed!");
-        }
-        return 5 + Math.random() * 20;
+
+        return 2 + Math.random() * 22;
     }
 
 }
--- src/main/java/concurrent/future/FutureDemo01.java
+++ src/main/java/concurrent/future/FutureDemo01.java
@@ -1,7 +1,39 @@
 package concurrent.future;
 
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
 public class FutureDemo01 {
 
+    @Test
+    public void test1() throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        Future<String> future = es.submit(() -> {
+            // to do some work
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "compute result";
+        });
 
+        // 1. 阻塞方式获取异步计算结果
+        System.out.println(future.get());
+        System.out.println("End...");
+
+        //2. 轮询方式获取异步计算结果
+        //while (future.isDone()){
+        //    System.out.println(future.get());
+        //    System.out.println("End...");
+        //}
+
+    }
+
+    public void test2() throws ExecutionException, InterruptedException {
+
+
+    }
 
 }
