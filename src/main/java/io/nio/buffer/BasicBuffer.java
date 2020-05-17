264a2465c7405b6f02e7f0209131cd060b18018b
官国成 <guanguocheng365@gmail.com>
2020-05-17 12:42:45 +0800

    Java NIO

---
+++ src/main/java/io/nio/buffer/BasicBuffer.java
@@ -0,0 +1,19 @@
+package io.nio.buffer;
+
+import java.nio.IntBuffer;
+
+public class BasicBuffer {
+
+    public static void main(String[] args) {
+
+        IntBuffer intBuffer = IntBuffer.allocate(5);
+        intBuffer.put(1);
+        intBuffer.put(2);
+        intBuffer.put(3);
+        intBuffer.put(4);
+        intBuffer.put(5);
+//        intBuffer.put(6);
+
+    }
+
+}
